8a8536f424f22fd6065043f976eab31036668d50
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-07 21:37:27 +0700

    new folder looping and new file

---
+++ Python/C++/for.py
@@ -0,0 +1,6 @@
+#Membuat perulangan pada python
+# #===============================================#
+
+for i in range(1, 11): # Perulangan dari 1 hingga 10
+    print(i) # Menampilkan nilai i
+print("Perulangan selesai") # Menampilkan pesan setelah perulangan selesai  
