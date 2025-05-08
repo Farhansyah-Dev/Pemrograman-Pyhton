643b31e832475a5a7ef51e98615b2951fb6d297d
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-08 21:30:46 +0700

    add many file in folder struktur-data

---
+++ Python/Struktur-data/array.py
@@ -0,0 +1,11 @@
+arr = [10, 20, 30, 40, 50] #inilialisasi array
+
+#Menampilkan isi array menggunakan for loop
+for i in range(len(arr)):
+    print(f"Elemen ke-{i} = {arr[i]}") #menampilkan elemen array ke-i
+
+#Menampilkan isi array menggunakan while loop
+i = 0
+while i < len(arr):
+    print(f"Elemen ke-{i} = {arr[i]}") #menampilkan elemen array ke-i
+    i += 1 #increment i
---
+++ Python/Struktur-data/delete.py
@@ -0,0 +1,24 @@
+# Program array untuk menghapus data pada array
+# Menghapus elemen dari array
+# Menggunakan fungsi del untuk menghapus elemen dari array
+
+# Menggunakan fungsi remove untuk menghapus elemen dari array
+# Menggunakan fungsi pop untuk menghapus elemen dari array
+# Menggunakan fungsi clear untuk menghapus semua elemen dari array
+# Menggunakan fungsi len untuk menghitung jumlah elemen dalam array
+# Menggunakan fungsi index untuk mencari indeks dari elemen dalam array
+# Menggunakan fungsi count untuk menghitung jumlah kemunculan elemen dalam array
+# Menggunakan fungsi sort untuk mengurutkan elemen dalam array
+
+angka = [1, 2, 3, 4, 5] # inisialisasi array
+print(angka) # cetak array
+# [1, 2, 3, 4, 5]
+angka.remove(3) # menghapus elemen 3 dari array
+print(angka) # cetak array
+# [1, 2, 4, 5]
+angka.pop(2) # menghapus elemen pada indeks 2 dari array
+print(angka) # cetak array
+# [1, 2, 4]
+angka.clear() # menghapus semua elemen dari array
+print(angka) # cetak array
+# []
