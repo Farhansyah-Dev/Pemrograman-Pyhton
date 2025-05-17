c10fb8abd67cceee4aefed8e4d7d11611e803e0c
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-17 16:28:14 +0700

    edit folder pendahuluan

--- Python/1-pendahuluan/main.py
+++ Python/1-pendahuluan/main.py
@@ -4,3 +4,20 @@
 
 print ("Hey There!, Hello World!") #ini adalah sintaks untuk mencetak pesan ke layar
 
+#Varible pada python
+# // Variable
+#     // variable adalah tempat untuk menyimpan data    
+
+nama = "Python" #ini adalah sintaks untuk mendeklarasikan variable nama dengan tipe data string
+
+angka = 10 #ini adalah sintaks untuk mendeklarasikan variable angka dengan tipe data integer
+
+koma = 3.14 #ini adalah sintaks untuk mendeklarasikan variable koma dengan tipe data float
+
+#boolean = True #ini adalah sintaks untuk mendeklarasikan variable boolean dengan tipe data boolean
+# // Boolean
+#     // boolean adalah tipe data yang hanya memiliki dua nilai yaitu True dan False
+
+benar = True #ini adalah sintaks untuk mendeklarasikan variable benar dengan tipe data boolean
+
+salah = False #ini adalah sintaks untuk mendeklarasikan variable salah dengan tipe data boolean
