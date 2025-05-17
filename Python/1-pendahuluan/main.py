f57f92dd3d0e9da61f7d5dfed85ce7fb38c40573
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-17 16:38:32 +0700

    modify folder pendahuluan main.py

--- Python/1-pendahuluan/main.py
+++ Python/1-pendahuluan/main.py
@@ -5,15 +5,18 @@
 print ("Hey There!, Hello World!") #ini adalah sintaks untuk mencetak pesan ke layar
 
 #Varible pada python
-# // Variable
-#     // variable adalah tempat untuk menyimpan data    
+#Variable
+#variable adalah tempat untuk menyimpan data    
 
+#Text Type
 nama = "Python" #ini adalah sintaks untuk mendeklarasikan variable nama dengan tipe data string
 
+#Numeric Types
 angka = 10 #ini adalah sintaks untuk mendeklarasikan variable angka dengan tipe data integer
 
 koma = 3.14 #ini adalah sintaks untuk mendeklarasikan variable koma dengan tipe data float
 
+#bollean type
 #boolean = True #ini adalah sintaks untuk mendeklarasikan variable boolean dengan tipe data boolean
 # // Boolean
 #     // boolean adalah tipe data yang hanya memiliki dua nilai yaitu True dan False
@@ -21,3 +24,21 @@ koma = 3.14 #ini adalah sintaks untuk mendeklarasikan variable koma dengan tipe 
 benar = True #ini adalah sintaks untuk mendeklarasikan variable benar dengan tipe data boolean
 
 salah = False #ini adalah sintaks untuk mendeklarasikan variable salah dengan tipe data boolean
+
+#Urutan atau list type
+#list
+buah = ['anggur','semangka','mangga']
+
+#tuple
+buah = ('anggur','semangka','mangga')
+
+#range
+urutan = range(1,10)
+
+#tipe data dictionary
+#dictionary
+usernamme = {
+    "nama" : "Muhammad Farhan Wirdiansyah",
+    "umur" : 20,
+    "alamat" : "Kp Cisalak 003/004 Kel Sumur Batu, Bantargebang Kota bekasi",
+}
