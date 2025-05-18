6b72576ec3327b6a320c9fa9c83ff6d98324b0ca
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-18 10:41:13 +0700

    create new file main.py

---
+++ Python/5-struktur-data/main.py
@@ -0,0 +1,18 @@
+#JENIS JENIS STRUKTUR DATA
+# - ARRAY : Struktur data linear yang menyimpan elemen dengan ukuran tetap
+# - list : struktur data dinamis yang dapat menyimpan elemen dengan ukuran yang bervariasi
+# - Stack : Struktur data berbasis LIFO (Last in, First Out)
+# - Queue : Struktur data berbasis FIFO (First In, First Out)
+# - Dictionary : Struktur data yang menyimpan pasangan key-value
+
+#CONTOH LIST DI PYTHON
+arr = [10, 20, 30, 40, 50] #inisialisasi list dengan nilai awal
+#menampilkan elemesn list dengan for
+for i in range(len(arr)):
+    print(f'uruta ke-{i}: {arr[i]}')
+    
+#OPERASI DASAR PADA LIST 
+# menambah elemen : menambahkan elemen baru kedalam list
+# menghapus elemen : menghapus elemen tertentu dari list
+# mengakses elemen : mengakses elemen tertentu dari list
+
