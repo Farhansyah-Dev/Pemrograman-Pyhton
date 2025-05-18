b956b206135e9ba3b37b434b16c29a67e80c7599
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-18 10:53:31 +0700

    menyempurnakan file main.py yang berisikan code list

--- Python/5-struktur-data/main.py
+++ Python/5-struktur-data/main.py
@@ -16,3 +16,30 @@ for i in range(len(arr)):
 # menghapus elemen : menghapus elemen tertentu dari list
 # mengakses elemen : mengakses elemen tertentu dari list
 
+angka = [] #deklarasi list kosong
+
+#menambahkan elemen kedalam list kosong
+angka.append(1) #menambahkan elemen 1
+angka.append(2) #menambahkan elemen 2
+angka.append(3) #menambahkan elemen 3
+
+#menampilkan elemen list
+print (f'Elemen dalam list: {angka}')
+
+#menambah elemen list lagi
+angka.append(5) #menambahkan elemen 4
+print(f'Setelah menambah elemen 4: {angka}')
+
+#MENGAKSES ELEMEN LIST
+angka_pertama = angka[0]
+angka_kedua = angka[1]
+angka_ketiga = angka [2]
+angka_terakhir = angka[-1]
+#menampilkan elemen yang diakses
+print(f'Angka Pertama: {angka_pertama}')
+print(f'Angka Kedua: {angka_kedua}')
+print(f'Angka Ketiga: {angka_ketiga}')
+print(f'Angka Terakhir: {angka_terakhir}')
+
+angka[-1] = 4 #mengubah nilai elemen terakhir
+print(f'Elemen list setelah diubah: {angka}')
