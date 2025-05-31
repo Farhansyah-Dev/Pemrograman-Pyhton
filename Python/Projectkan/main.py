70ed10e05008b835c8e12aeb567ae8bd8a95d62c
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-05-31 21:43:52 +0700

    membuat folder projeckan

---
+++ Python/Projectkan/main.py
@@ -0,0 +1,23 @@
+from libs import welcome_message, exit_game
+from games import monster
+from tools import warung
+
+def menu():
+    user_option = int(input(f'\nmenu program:\n1.  Games Monster Goa\n2. Warung Mini\n3. Keluar\n\nsilahkan pilih: '))
+    
+    if user_option == 1:
+        monster.start()
+    elif user_option == 2:
+        warung.start()
+    elif user_option == 3:
+        exit_game()
+        exit()
+    else:
+        print('pilih menu yang tersedia. ')
+        
+def main():
+    welcome_message()
+    menu()
+    
+if __name__=='__main__':
+    main()
